10d086bc9a926ac89f1602820b17d715186e0282
Prakhya Chaturvedi <prakhyachaturvedi@gmail.com>
2026-07-12 14:15:37 +0530

    Remove node_modules, dist and .env from Fleet Manager

--- Fleet Manager/backend/dist/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dashboardController_1 = require("../controllers/dashboardController");
-const auth_1 = require("../middleware/auth");
-const router = (0, express_1.Router)();
-router.get('/', auth_1.authenticateJWT, dashboardController_1.getDashboardData);
-exports.default = router;
